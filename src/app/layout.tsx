9509893888083fc8457e8d580a28f622583b5337
luigiBilhon <luigi.jordanio@bilhon.tech>
2025-08-15 00:21:49 -0300

    feat: add app icons and manifest for improved PWA support

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import {
 import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
 import { Header } from '@/components/smart/Header/Header'
+import Head from 'next/head'
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -36,8 +37,56 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+        <Head>
+          <title>Questões CPNU</title>
+          <base href="/" />
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+
+          <meta charSet="UTF-8" />
+          <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+          <meta content="Prepare-se para o sucesso no seu próximo exame com Questões CPNU." name="description" />
+          <meta content="exame, preparação, simulados, feedback, estudo" name="keywords" />
+          <meta content="8 Pixel Design" name="author" />
+          <meta content="index, follow" name="robots" />
+          <meta content="Questões CPNU - Prepare-se para o sucesso no seu próximo exame" property="og:title" />
+          <meta content="Personalize seus simulados e obtenha feedback instantâneo para melhorar seus resultados."
+            property="og:description" />
+          <meta content="capa.png" property="og:image" />
+          <meta content="https://questoes.cpnu.com.br" property="og:url" />
+          <meta content="capa.png" name="twitter:card" />
+          <meta content="@luigi_flu" name="twitter:site" />
+          <meta content="Personalize seus simulados e obtenha feedback instantâneo para melhorar seus resultados."
+            name="twitter:description" />
+          <meta content="favicon-32x32.png" name="twitter:image" />
+          <meta content="website" property="og:type" />
+          <meta content="pt_BR" property="og:locale" />
+          <meta content="1200" property="og:image:width" />
+          <meta content="675" property="og:image:height" />
+          <meta content="Questões CPNU" property="og:site_name" />
+          <meta content="Logo da Questões CPNU" name="twitter:image:alt" />
+          <meta content="Questões CPNU - Prepare-se para o sucesso no seu próximo exame" name="twitter:title" />
+          <meta content="Personalize seus simulados e obtenha feedback instantâneo para melhorar seus resultados."
+            name="twitter:description" />
+          <link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png" />
+          <link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png" />
+          <link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png" />
+          <link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png" />
+          <link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png" />
+          <link rel="icon" type="image/png" sizes="192x192" href="/android-icon-192x192.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+          <link rel="manifest" href="/manifest.json" />
+          <meta name="msapplication-TileColor" content="#ffffff" />
+          <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+          <meta name="theme-color" content="#ffffff" />
+        </Head>
         <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-           { isShowHeader && <Header />}
+          {isShowHeader && <Header />}
           {children}
         </body>
       </html>
