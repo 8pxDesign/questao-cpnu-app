e456a962adaeeb6f12f0afb58cb3dfe0fba9b1bf
jordaniodev <luigijordanio@gmail.com>
2025-09-03 11:42:32 -0300

    feat: Refactor AuthModal and Header components to improve authentication flow and modal handling

--- src/app/(components)/AuthModal/index.tsx
+++ src/app/(components)/AuthModal/index.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { Dialog, DialogClose, DialogContent } from "@/components/ui/dialog";
+import { Dialog, DialogContent } from "@/components/ui/dialog";
 
-import { SignIn } from "@clerk/nextjs";
+import { SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
 import { DialogTitle } from "@radix-ui/react-dialog";
 import { useAuthModal } from "./index.hook";
 
@@ -12,11 +12,18 @@ export const AuthModal = () => {
     const completeActualURL = typeof window !== 'undefined' ? `${window.location.pathname}?${window.location.search}` : '/';
     if (!isOpen) return null;
     return (
-        <Dialog open={isOpen} >
-            <DialogTitle className="hidden">Entrar</DialogTitle>
-            <DialogContent showCloseButton={false} className="flex flex-col p-[0] w-[400px] sm:max-w-[calc(-2.5rem_+_100vw)] bg-transparent border-0 items-center">
-                <SignIn routing="hash" forceRedirectUrl={completeActualURL ?? '/'} />
-            </DialogContent>
-        </Dialog>
+        <>
+            <SignedOut>
+                <Dialog open={isOpen} >
+                    <DialogTitle className="hidden">Entrar</DialogTitle>
+                    <DialogContent showCloseButton={false} className="flex flex-col p-[0] w-[400px] sm:max-w-[calc(-2.5rem_+_100vw)] bg-transparent border-0 items-center">
+                        <SignIn routing="hash" forceRedirectUrl={completeActualURL ?? '/'} />
+                    </DialogContent>
+                </Dialog>
+            </SignedOut>
+            <SignedIn>
+                <div className="hidden"></div>
+            </SignedIn>
+        </>
     )
 }
--- src/components/smart/Header/Header.tsx
+++ src/components/smart/Header/Header.tsx
@@ -1,9 +1,17 @@
+'use client'
 import { Button } from "@/components/ui/button"
 import { SignedIn, SignedOut, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs"
 import { CountQuestion } from "../CountQuestion/CountQuestion"
 import Link from "next/link"
+import { useAuthModal } from "@/app/(components)/AuthModal/index.hook"
 
 export const Header = () => {
+    const { openModal } = useAuthModal();
+
+    const signIn = () => {
+        openModal();
+    }
+
     return <header className="w-full bg-sidebar-background items-center sticky top-0 z-10">
         <div className="flex justify-end items-center p-4 gap-4 max-w-[1280px] mx-auto">
             <div className="flex items-center gap-4 justify-between w-full">
@@ -12,12 +20,7 @@ export const Header = () => {
                 </Link>
                 <SignedOut>
                     <div className="flex gap-[16px]">
-                        <SignInButton >
-                            <Button variant={'secondary'} size={'sm'} >Entrar</Button>
-                        </SignInButton>
-                        <SignUpButton>
-                            <Button variant={'default'} size={'sm'}>Cadastrar</Button>
-                        </SignUpButton>
+                        <Button variant={'secondary'} size={'sm'} onClick={signIn}>Entrar</Button>
                     </div>
                 </SignedOut>
                 <SignedIn>
