e5f0a4d1d4dfd13cc1c4de736eb15e51095e61e6
luigiBilhon <luigi.jordanio@bilhon.tech>
2025-09-08 09:56:18 -0300

    feat: add question challenge component and update page data structure
    - Created a new type definition for QuestionProps in index.type.ts.
    - Updated page.data.tsx to include additional question blocks and structured data for questions.
    - Modified page.tsx to integrate the QuestionChallenge component, displaying the block and questions dynamically.
    - Refactored question component to use a consistent type for questions, enhancing type safety.

--- src/app/(home)/desafio/[id]/page.tsx
+++ src/app/(home)/desafio/[id]/page.tsx
@@ -1,6 +1,9 @@
 import { Alternative } from "@/types/Alternative";
 import { Question } from "@/types/Question";
 import { Topic } from "@/types/Topic";
+import { QUESTIONS } from "./page.data";
+import { QuestionChallenge } from "./(components)/QuestionChallenge";
+import { Block } from "@/types/Block";
 
 interface DesafioPageProps {
     params: Promise<{
@@ -8,18 +11,35 @@ interface DesafioPageProps {
     }>;
 }
 
+export interface AlternativeQuestionChallenge extends Pick<Alternative, 'id' | 'description' | 'position' | 'correctAnswer'> {}
 export interface QuestionChallenge extends Pick<Question, 'statement' | 'comment'> {
-    alternatives: Array<Pick<Alternative, 'id' | 'description' | 'position' | 'correctAnswer'>>;
+    alternatives: Array<AlternativeQuestionChallenge>;
     topic: Pick<Topic, 'name'>;
 }
 export interface QuestionChallengeData {
     [key: number]: {
         questions: Array<QuestionChallenge>
+        bloco: Pick<Block, 'id' | 'name'>;
     }
 }
 
 export default async function Page({ params }: DesafioPageProps) {
+    const id = Number((await params).id);
+    const questionData = QUESTIONS[id];
+    const questoes = questionData?.questions || [];
 
-
-
+    return <>
+        <div className="w-full h-[140px] bg-sky-50 flex justify-center">
+            <div className="max-w-[600px] px-[16px] ">
+                <div className="flex items-center h-[140px]">
+                    <div className="flex flex-col items-start flex-1">
+                        <p className="text-blue-600 md:text-2xl text-xl font-black uppercase leading-normal">Desafio Simula PRO  <span className="text-green-400">2025</span></p>
+                        <p className="px-3 py-1.5 bg-sky-100 rounded-2xl font-bold inline-flex justify-center items-center gap-2 text-blue-600 text-xs leading-none">Bloco {id}</p>
+                    </div>
+                    <img src="/images/challenge.svg" alt="Desafio Simula PRO" />
+                </div>
+            </div>
+        </div>
+        <QuestionChallenge bloco={questionData.bloco} questions={questoes} />
+    </>
 }
